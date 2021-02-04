312cce3a345d7125ed6c5f0d7beccc08f443b60e
iambariz <61651210+iambariz@users.noreply.github.com>
2021-02-04 22:32:15 +0000

    Update data.js
    Data test

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+/*
 class item{
   constructor(name, id, price, inStock, unit, type,) {
       this.name = name;
@@ -16,18 +17,88 @@ class item{
 }
 
 
-
-
-
 let shopItems = [
 
-];
+itemOne = new item ("Fairtrade Bananas Loose", 1, 1, 23, 1, "fruit"),
+itemTwo = new item ("British apple", 2, 0.79, 23, 1, "fruit"),
+itemThree = new item ("British Minced beef", 3, 0.79, 243, 1, "meat"),
+itemFour = new item ("British chicken breast", 4, 0.79, 233, 1, "meat"),
+itemFive = new item ("Lynx Black", 5, 0.99, 0, 1, "deodorant"),
+itemSix = new item ("Old spice Blue Winter", 6, 0.79, 3, 1, "deodorant"),
+itemSeven = new item ("Paracetamol", 7, 1.79, 1, 1, "drug"),
+itemEight = new item ("Nurofen", 8, 2.49, 6, 1, "drug"),
 
-let itemOne = new item ("Fairtrade Bananas Loose", 1, 1, 23, 1, "fruit");
-let itemTwo = new item ("British apple", 2, 0.79, 23, 1, "fruit");
-let itemThree = new item ("British Minced beef", 3, 0.79, 243, 1, "meat");
-let itemFour = new item ("British chicken breast", 4, 0.79, 233, 1, "meat");
-let itemFive = new item ("Lynx Black", 5, 0.99, 0, 1, "deodorant");
-let itemSix = new item ("Old spice Blue Winter", 6, 0.79, 3, 1, "deodorant");
-let itemSeven = new item ("Paracetamol", 7, 1.79, 1, 1, "drug");
-let itemEight = new item ("Nurofen", 8, 2.49, 6, 1, "drug");
+]
+*/
+
+const items = [{
+    id: 1,
+    title: "Banana",
+    category: "fruits",
+    price: 13.99,
+    img: "./images/banana.jpg",
+  },
+  {
+    id: 2,
+    title: "Apple",
+    category: "fruits",
+    price: 4.99,
+    img: "./images/apple.jpg",
+  },
+  {
+    id: 3,
+    title: "Chicken Breast",
+    category: "meat",
+    price: 13.99,
+    img: "./images/chicken.jpg",
+  },
+  {
+    id: 4,
+    title: "Beef",
+    category: "meat",
+    price: 16.99,
+    img: "./images/beef.jpg",
+  },
+  {
+    id: 5,
+    title: "Pear",
+    category: "fruits",
+    price: 9.99,
+    img: "./images/pear.jpg",
+  },
+  {
+    id: 6,
+    title: "Leek",
+    category: "vegetables",
+    price: 12.99,
+    img: "./images/leek.jpg",
+  },
+  {
+    id: 7,
+    title: "Potato",
+    category: "vegetables",
+    price: 11.99,
+    img: "./images/potato.jpg",
+  },
+  {
+    id: 8,
+    title: "Tomato",
+    category: "vegetables",
+    price: 7.99,
+    img: "./images/tomato.jpg",
+  },
+  {
+    id: 9,
+    title: "Milk",
+    category: "Milk",
+    price: 6.99,
+    img: "./images/milk.jpg",
+  },
+  {
+    id: 10,
+    title: "Eggs",
+    category: "eggs",
+    price: 13.99,
+    img: "./images/eggs.jpg",
+  },
+]

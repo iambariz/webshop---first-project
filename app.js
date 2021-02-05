2be1d0734b774e328aa93828f4ac0afad433ce8c
iambariz <61651210+iambariz@users.noreply.github.com>
2021-02-05 17:09:51 +0000

    Update app.js
    Function works, however I need to put rows somehow

---
+++ app.js
@@ -0,0 +1,30 @@
+const section = document.querySelector(".item-row");
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    displayItems(items)
+})
+
+//display
+function displayItems(array) {
+    let display = array.map(function (item) {
+        return `
+    <div class="item-card-test">
+        <div class="card-wrapper">
+            <img class="image card-image" src="${item.img}" alt="product">
+            <p class="test-name">${item.title}</p>
+            <p class="price">
+                <span class="full-price">${item.price}</span>
+                <span class="unit-price secondary-info">£13.89/kg </span></p>
+            <p class="purchase">
+                <input type="number" class="quantity" name="quantity" min="1" max="5">
+                <a class="btn btn-submit" href="#">Add</a>
+            </p>
+        </div>
+    </div>
+`;
+    });
+    display = display.join("");
+    section.innerHTML = display;
+    console.log("Works")
+}

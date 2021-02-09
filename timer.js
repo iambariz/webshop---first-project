8c27a03ca614edec4a68d6142f6d7cbf3c933713
iambariz <61651210+iambariz@users.noreply.github.com>
2021-02-09 12:53:10 +0000

    Update timer.js
    Display added, calculator added

--- timer.js
+++ timer.js
@@ -4,7 +4,9 @@ let isRunning = false;
 
 let startTime;
 let endTime;
-let timeLeft
+let timeLeft;
+let minutesLeft;
+let secondsLeft;
 
 function getTimes() {
     isRunning = true;
@@ -14,4 +16,27 @@ function getTimes() {
     endtime = endTime.setMinutes(endTime.getMinutes() + 30);
 }
 
+function getMS() {
+    timeLeft = endTime.getTime() - startTime.getTime();
+    console.log(timeLeft);
+    minutesLeft = Math.floor(timeLeft / 60000);
+    secondsLeft = (timeLeft - (minutesLeft * 60000));
+    console.log(minutesLeft);
+    displayTime(minutesLeft, secondsLeft);
+}
+
+function displayTime(min, sec) {
+    if (min < 10) {
+        minuteDisplay.textContent = "0" + min;
+    } else {
+        minuteDisplay.textContent = min;
+    }
+    if (sec < 10) {
+        secondDisplay.textContent = "0" + sec;
+    } else {
+        secondDisplay.textContent = sec;
+    }
+}
+
 getTimes();
+getMS();

0d5bb319da8563f87a584966673b97f69cbd357a
iambariz <61651210+iambariz@users.noreply.github.com>
2021-02-09 14:47:38 +0000

    Update Timer.JS
    Finished, only styling left

--- timer.js
+++ timer.js
@@ -1,5 +1,7 @@
 const secondDisplay = document.querySelector(".second");
 const minuteDisplay = document.querySelector(".minute");
+
+
 let isRunning = false;
 
 let startTime;
@@ -9,20 +11,21 @@ let minutesLeft;
 let secondsLeft;
 
 function getTimes() {
-    isRunning = true;
     startTime = new Date();
-    startTime.getTime();
     endTime = new Date(startTime);
-    endtime = endTime.setMinutes(endTime.getMinutes() + 30);
+    endTime.setMinutes(startTime.getMinutes() + 30);
+    startTime.getTime();
+    timeLeft = endTime.getTime() - startTime.getTime();
+    getMS();
 }
 
 function getMS() {
-    timeLeft = endTime.getTime() - startTime.getTime();
-    console.log(timeLeft);
     minutesLeft = Math.floor(timeLeft / 60000);
-    secondsLeft = (timeLeft - (minutesLeft * 60000));
+    secondsLeft = (timeLeft - (minutesLeft * 60000)) / 1000;
     console.log(minutesLeft);
+    console.log(secondsLeft);
     displayTime(minutesLeft, secondsLeft);
+    timeLeft = timeLeft - 1000;
 }
 
 function displayTime(min, sec) {
@@ -38,5 +41,21 @@ function displayTime(min, sec) {
     }
 }
 
-getTimes();
-getMS();
+function myTimer() {
+    console.log("works");
+    if (isRunning == false) {
+        isRunning = true;
+        getTimes();
+    }
+    if (timeLeft > 0) {
+        getMS()
+    } else {
+        stopTimer();
+    }
+}
+
+let timer = setInterval(myTimer, 1000);
+
+function stopTimer(func) {
+    clearInterval(func);
+}

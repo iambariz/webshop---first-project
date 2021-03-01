1b571831ce86805b594be7696ad7d9b6f31db5ee
iambariz <61651210+iambariz@users.noreply.github.com>
2021-03-01 22:28:07 +0000

    Update JS
    Login added

--- login.js
+++ login.js
@@ -1,3 +1,22 @@
 userInput = document.querySelector("#userName");
 userPw = document.querySelector("#password");
 errorMsg = document.querySelector(".error-msg");
+loginButton = document.querySelector(".btn-login");
+
+
+function updateMsg() {
+
+}
+
+function checkUser() {
+    let userNames = getUserStorage();
+    for (let i = 0; i < userNames.length; i++) {
+        if (userNames[i].userName == userInput.value &&
+            userNames[i].userName == userPw.value) {
+            //Login comes here
+            break;
+        }
+    }
+}
+
+loginButton.addEventListener('click', checkUser);

57a5a5bd933342cb06c9dcfed52eb9392c71a7d0
iambariz <61651210+iambariz@users.noreply.github.com>
2021-03-02 23:12:06 +0000

    Update HTML
    Users.html Added

--- user.js
+++ user.js
@@ -10,7 +10,7 @@ function checkLogin() {
     if (loggedInList[0].loggedIn == true) {
         loginMenu.style.display = "none";
         userDisplay.style.display = "inline";
-        userDisplay.textContent = loggedInList[0].firstName + " " + loggedInList[0].lastName;
+        userDisplay.children[0].textContent = loggedInList[0].firstName + " " + loggedInList[0].lastName;
         console.log(loggedInList[0].firstName)
     }
 }

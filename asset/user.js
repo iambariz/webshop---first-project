07e34ff522e0008fea5ae0e07df3e15b5f9e4a8c
iambariz <61651210+iambariz@users.noreply.github.com>
2021-03-05 22:36:24 +0000

    Minor bugfixes

--- asset/user.js
+++ asset/user.js
@@ -8,15 +8,14 @@ const openNav = document.querySelector(".fas.fa-bars");
 //REPAIR LATER !! IT HAS TO BE UNDEFENIED
 function checkLogin() {
     loggedInList = loggedInUser();
-    if (loggedInList[0].loggedIn == undefined || loggedInList[0].loggedIn == false) {
-        loginMenu.style.display = "inline";
-        userDisplay.style.display = "none";
-    }
-    if (loggedInList[0].loggedIn == true) {
+    if (loggedInList.length > 0) {
         loginMenu.style.display = "none";
         userDisplay.style.display = "inline";
         userDisplay.children[0].textContent = loggedInList[0].firstName + " " + loggedInList[0].lastName;
         console.log(loggedInList[0].firstName)
+    } else {
+        loginMenu.style.display = "inline";
+        userDisplay.style.display = "none";
     }
 }
 

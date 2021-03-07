29086a1792854511dbf82bd7029a52cbe796369b
iambariz <61651210+iambariz@users.noreply.github.com>
2021-03-07 22:46:54 +0000

    Update JS
    Event listeners, and variables added

---
+++ asset/register.js
@@ -0,0 +1,10 @@
+//Selectors
+const userField = document.querySelector('.userName-Reg');
+const pwField = document.querySelector('.password-Reg');
+const regBtn = document.querySelector('.btn-register');
+const firstNum = document.querySelector('.first-num');
+const secondNum = document.querySelector('.second-num');
+const operator = document.querySelector('.operator');
+
+const operators = ["*", "-", "+"];
+let toSolve = [];

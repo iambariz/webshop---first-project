8c83274f81e717582e0fb5604988471445f6539b
iambariz <61651210+iambariz@users.noreply.github.com>
2021-03-12 20:48:45 +0000

    Update JS
    Display function added

--- asset/userDisplay.js
+++ asset/userDisplay.js
@@ -2,6 +2,36 @@
 
 const firstNameDisplays = document.querySelectorAll(".user-first");
 const surNameDisplays = document.querySelectorAll(".user-second");
-
+const adressField = document.querySelector(".user-adress");
+const logOutBtn = document.querySelector(".sign-out");
+const editUserBtn = document.querySelector(".edit");
+const doneBtn = document.querySelector(".done");
 
 let editMode = false;
+
+function editToggler() {
+    if (editMode == false) {
+        editMode == true;
+        toggleDisplay(doneBtn, editUserBtn);
+    }
+    if (editMode == true) {
+        editMode == false;
+        toggleDisplay(doneBtn, editUserBtn);
+    }
+}
+
+function toggleDisplay(x, y) {
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+
+editUserBtn.addEventListener('click', editToggler);
+doneBtn.addEventListener('click', editToggler);
